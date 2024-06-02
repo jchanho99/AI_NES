82cac255e8f3f241adf650353d71a869bbbe130e
jchanho99 <116345531+jchanho99@users.noreply.github.com>
2024-06-03 03:34:37 +0900

    added date function to update daily

--- AI/data_preprocessing.py
+++ AI/data_preprocessing.py
@@ -1,11 +1,14 @@
-from connect import init_db
+import datetime
 import pandas as pd
+from connect import init_db
 
 # Connect to db
+now = datetime.datetime.now().date()
+date = now.strftime("%Y_%m_%d")
+ref = init_db('2024_06_01')
 
-def load_news(date): # date = '2024년05월22일 , 10시26분56초 네이버 기사 헤드라인'
+def load_news(): # date = '2024년05월22일 , 10시26분56초 네이버 기사 헤드라인'
     # Collecting all news data from db
-    ref = init_db(date)
     data_ref = ref.get()
     
     df = pd.DataFrame()
@@ -16,12 +19,10 @@ def load_news(date): # date = '2024년05월22일 , 10시26분56초 네이버 기
 
     return df['summary']
 
-def save_sentiment(date, sentiment):
-    ref = init_db(date)
-    # data_ref = ref.get()
-    
+def save_sentiment(sentiment):
     for i, v in enumerate(sentiment):
+        child_ref = ref.child(f"{i}")
         sent = {
             'sentiment': v
         }
-        ref[i].set(sent)
+        child_ref.update(sent)
